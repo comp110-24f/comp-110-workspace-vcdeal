9c812c4711fc9a17da462513bda52729ced41d17
Victoria Deal <vdeal@unc.edu>
2024-09-16 11:52:34 -0400

    finshed practice with loops

---
+++ lessons/diagrams/characters.py
@@ -0,0 +1,11 @@
+"""Practice with loops"""
+
+
+def characters(msg: str) -> None:
+    index: int = 0
+    while index < len(msg):
+        print(msg[index])
+        index = index + 1
+
+
+characters(msg="Howdy")
