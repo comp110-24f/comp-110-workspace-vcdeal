18fac8c24d0e411b17c9b16de0d8243fbb5f8541
vcdeal <vdeal@unc.edu>
2024-09-27 23:26:15 -0400

    almost finished ex03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,5 +1,7 @@
 """Creating my own Wordle game!"""
 
+__author__: str = "730745012"
+
 
 def input_guess(length: int) -> str:
     """Asks user to input a word"""
@@ -47,19 +49,27 @@ def emojified(guess: str, secret: str) -> str:
 
 def main(secret: str) -> None:
     """The entrypoint of the program"""
-    turns: int = 0  # tracks number of turns the user has used
-    while turns <= 6:  # turns cannot be more than 7
+    turns: int = 1  # tracks number of turns the user has used
+    game: bool = False  # tracks if the user has won or not
+    while (
+        turns <= 6 and game is False
+    ):  # allows up to 6 turns if the user has not already won
         print(f"=== Turn {turns}/6 ===")
+        guess: str = input_guess(
+            length=len(secret)
+        )  # prompts the user for a guess and stores the answer
         print(
-            emojified(guess=input_guess(length=len(secret)), secret=secret)
-        )  # prints results of emojified, prompts user for a guess during this line
-        if (
-            input_guess(length=len(secret)) == secret
-        ):  # if the user's guess is the secret word
-            # what should i do here?       
+            emojified(guess=guess, secret=secret)
+        )  # prints results of emojified using the user's guess as an arg
+        if guess == secret:  # checks if the guess matches the secret word
+            game = True  # game has been won
         else:
             turns += 1
-    if input_guess(length=len(secret)) == secret:
-        print(f"You won in {turns}/turns!")
-    else:
-        print("X/6 - Sorry, try again tomorrow!")
+    if game is True:  # if the game has been won
+        print(f"You won in {turns}/6 turns!")  # prints win message
+    else:  # if the game has been lost
+        print("X/6 - Sorry, try again tomorrow!")  # prints try-again message
+
+
+if __name__ == "__main__":
+    main(secret="codes")
