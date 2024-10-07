299eca5e9c1e4611040081e44c3083df43084c17
vcdeal <vdeal@unc.edu>
2024-10-07 12:04:02 -0400

    actually finished the lesson

--- lessons/for_loops.py
+++ lessons/for_loops.py
@@ -4,3 +4,8 @@ pets: list[str] = ["Louie", "Bo", "Bear"]
 
 for puppy in pets:
     print(f"Good boy, {puppy}!")
+
+names: list[str] = ["Alyssa", "Janet", "Vrinda"]
+
+for idx in range(0, len(names)):
+    print(f"{idx}: {names[idx]}")
