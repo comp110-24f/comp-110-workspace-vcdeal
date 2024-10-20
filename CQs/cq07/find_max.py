3c3ec000d199b89576d37c0548cb32555e75a219
vcdeal <vdeal@unc.edu>
2024-10-19 21:46:20 -0400

    finished working on cq07

--- CQs/cq07/find_max.py
+++ CQs/cq07/find_max.py
@@ -9,11 +9,10 @@ def find_and_remove_max(input_list: list[int]) -> int:
         return -1
     largest_num: int = input_list[0]  # largest number is the first value in the list
     largest_idx: int = 0  # start index at the first value in the list
-    for idx in range(
-        1, len(input_list)
-    ):  # Loop through the list to find the largest number and its index
+    idx: int = 1  # start the index for the while loop at 1
+    while idx < len(input_list):
         if input_list[idx] > largest_num:
             largest_num = input_list[idx]
             largest_idx = idx
-    input_list.pop(largest_idx)  # pop the element at largest_idx
+            input_list.pop(largest_idx)  # pop the element at largest_idx
     return largest_num  # return the largest number
--- CQs/cq07/max_test.py
+++ CQs/cq07/max_test.py
@@ -1,4 +1,22 @@
 __author__ = "730745012"
 
+from CQs.cq07.find_max import find_and_remove_max
 
-def test_find_and_remove_max(input_lis): list[int[]]
+
+def test_find_and_remove_max_return_case() -> None:
+    """Ensures that find_and_remove_max returns the expected value"""
+    a: list[int] = [1, 2, 3]
+    assert find_and_remove_max(a) == 3
+
+
+def test_find_and_remove_max_mutate_case() -> None:
+    """Ensures that find_and_remove_max mutates the list"""
+    a: list[int] = [1, 2, 3]
+    find_and_remove_max(a)
+    assert a == [1, 2]
+
+
+def test_find_and_remove_max_edge_case() -> None:
+    """Ensures that find_and_remove_max returns the correct input when
+    the list is empty"""
+    assert find_and_remove_max([]) == -1
