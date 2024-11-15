76ca64178eee2c6c113524504e1f07a915e5c0d4
vcdeal <vdeal@unc.edu>
2024-11-14 21:41:40 -0500

    finished practice w recursions

---
+++ lessons/linked_list.py
@@ -0,0 +1,51 @@
+"""Practice w/ Recursions."""
+
+from __future__ import annotations
+
+
+class Node:
+    value: int
+    next: Node | None
+
+    def __init__(self, value: int, next: Node | None):
+        self.value = value
+        self.next = next
+
+    def __str__(self) -> str:
+        """Produce a string represetnation of a linked list."""
+        rest: str = "TODO"
+        # TODO: figure out the rest of the list
+        if self.next is None:
+            rest = "None"
+        else:
+            rest = self.next.__str__()  # same as str(self.next)
+        return f"{self.value} -> {rest}"
+
+
+two: Node = Node(2, None)
+one: Node = Node(1, two)
+courses: Node = Node(110, Node(210, Node(301, None)))
+
+
+def to_str(head: Node | None) -> str:
+    """Represent a Linked List as a str."""
+    if head is None:
+        return "None"
+    else:
+        rest: str = to_str(head.next)
+        return f"{head.value} -> {rest}"
+
+
+def last(head: Node) -> int:
+    """Return the last value of a non-empty list."""
+    # Base Case: when head is the "last" node
+    if head.next is None:
+        return head.value
+    else:
+        # Recursive case:
+        rest: int = last(head.next)
+        return rest
+
+
+print(last(one))  # Expect to print 2
+print(last(courses))  # Expect to print 301
